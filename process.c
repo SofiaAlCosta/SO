7bd9cf390e0f57e03e6b05d78ce25f21e574452f
xandexun <alexandre.axe0246@gmail.com>
2025-04-20 19:51:23 +0100

    função generate_random_processes atualizada

--- process.c
+++ process.c
@@ -1,59 +1,93 @@
 #define _USE_MATH_DEFINES
+#include <math.h>
 #include "process.h"
 #include <time.h>
-#include <math.h>
+#include <stdlib.h>
+#include <stdio.h>
 
 #ifndef M_PI
 #define M_PI 3.14159265358979323846
 #endif
 
-// Distribuição exponencial (para arrival time)
+// Distribuição exponencial
 double rand_exponential(double lambda) {
-    double u = (double) rand() / RAND_MAX;
-    return -log(1 - u) / lambda;
+    if (lambda <= 0) return 1.0;
+    double u = (double)rand() / (RAND_MAX + 1.0);
+    return -log(1.0 - u) / lambda;
 }
 
-// Distribuição normal (Box-Muller)
+// Distribuição normal
 double rand_normal(double mean, double stddev) {
-    double u1 = (double) rand() / RAND_MAX;
-    double u2 = (double) rand() / RAND_MAX;
+    double u1 = (double)rand() / (RAND_MAX + 1.0);
+    double u2 = (double)rand() / (RAND_MAX + 1.0);
     double z = sqrt(-2.0 * log(u1)) * cos(2 * M_PI * u2);
     return z * stddev + mean;
 }
 
-// Geração de processos com valores fixos ou aleatórios simples
+// Geração de processos estáticos
 Process* generate_static_processes(int count) {
+    if (count <= 0) return NULL;
     Process* list = malloc(sizeof(Process) * count);
+    if (!list) {
+        fprintf(stderr, "Erro: Falha ao alocar memória em generate_static_processes\n");
+        return NULL;
+    }
     for (int i = 0; i < count; i++) {
         list[i].id = i + 1;
         list[i].arrival_time = i * 2;
         list[i].burst_time = 5 + (i % 3);
+        if (list[i].burst_time <=0) list[i].burst_time = 1;
         list[i].remaining_time = list[i].burst_time;
         list[i].priority = 1 + (i % 5);
-        list[i].deadline = list[i].arrival_time + 10;  // exemplo
+        list[i].deadline = list[i].arrival_time + 10 + (rand() % 5);
         list[i].period = 0;
     }
     return list;
 }
 
-// Geração de processos com distribuições probabilísticas
-Process* generate_random_processes(int count, int max_arrival, int max_burst) {
+Process* generate_random_processes(int count, double lambda, double mean, double stddev) {
+    if (count <= 0) return NULL;
     Process* list = malloc(sizeof(Process) * count);
+     if (!list) {
+        fprintf(stderr, "Erro: Falha ao alocar memória em generate_random_processes\n");
+        return NULL;
+    }
     double current_time = 0.0;
 
     for (int i = 0; i < count; i++) {
         list[i].id = i + 1;
 
-        current_time += rand_exponential(0.5);
-        list[i].arrival_time = (int) current_time;
+        // Tempo de Chegada
+        current_time += rand_exponential(lambda);
+        list[i].arrival_time = (int)round(current_time);
+        if (list[i].arrival_time < 0) list[i].arrival_time = 0;
 
-        int burst = (int) rand_normal(5.0, 2.0);
+        // Tempo de Burst
+        int burst = (int)round(rand_normal(mean, stddev));
         list[i].burst_time = burst > 0 ? burst : 1;
         list[i].remaining_time = list[i].burst_time;
 
-        list[i].priority = 1 + rand() % 5;
-        list[i].deadline = list[i].arrival_time + 10 + rand() % 10;
-        list[i].period = 0; // por agora não são periódicos
+        // Prioridade
+        int dice = rand() % 100;
+        if (dice < 40) {
+            list[i].priority = 1;
+        } else if (dice < 70) {
+            list[i].priority = 2;
+        } else if (dice < 90) {
+            list[i].priority = 3;
+        } else if (dice < 97) {
+            list[i].priority = 4;
+        } else {
+            list[i].priority = 5;
+        }
+
+        list[i].deadline = list[i].arrival_time + (int)round(2.0 * mean) + (rand() % (int)(mean+1)) - (int)(mean/2.0);
+         if (list[i].deadline < list[i].arrival_time + list[i].burst_time) {
+             list[i].deadline = list[i].arrival_time + list[i].burst_time + 1;
+         }
+
+
+        list[i].period = 0;
     }
 
     return list;
